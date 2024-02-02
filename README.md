1f50e3ed8b83c3b0c6d0e1eddbaf02672897605a
Taylor Pubins <tpubz@icloud.com>
2024-02-01 17:37:28 -0700

    100 tests

--- README.md
+++ README.md
@@ -5,6 +5,11 @@
 Applet needs to source an ESPN Fantasy Baseball League Managers page given a league ID.  Base url is here https://fantasy.espn.com/baseball/tools/leaguemembers?leagueId={#lgid}.
 This webscraping applet should get the team name, abbreviation, owner, and avatar URL.  Output format is .json; see below for schema.
 
+## Requirements
+* MTBL ETL pipeline relies on a valid league ID and should be set in global environment vars
+  * add to ~/.bash_profile or ~/.bashrc or ~/.zshrc: `MTBL_LGID={#your_lgid}`
+  * verify it's existence with `echo $MTBL_LGID` from terminal
+  * using python's `os.getenv("MTBL_LGID")` you will have access at the app execution
 ## Dependencies
 * python >= 3.12
   * required in [setup.py]
--- app/manager_getter/manager_getter.py
+++ app/manager_getter/manager_getter.py
@@ -1,74 +1,70 @@
-# author: pubins.taylor
-# modified date: 30 JAN 2024
-# description: This module gets the manager data from ESPN Fantasy Baseball
-# v0.1.0
+"""
+author: pubins.taylor
+modified date: 1 FEB 2024
+description: This module gets the manager data from ESPN Fantasy Baseball
+v0.2.0
+"""
+import logging
 
+from pandas import DataFrame
 from selenium import webdriver
+from selenium.common import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import pandas as pd
-import os
 
-espn_lg_member_base_url = "https://fantasy.espn.com/baseball/tools/leaguemembers?leagueId="
+ESPN_LG_BASE_URL = "https://fantasy.espn.com/baseball/tools/leaguemembers?leagueId="
 
 
-def get_managers(driver: webdriver.Chrome, lg_id: str) -> None:
+def get_managers(driver: webdriver.Chrome, lg_id: str) -> DataFrame:
+    """
+    Get the managers for a given league
+    :param driver: pre-built selenium webdriver
+    :param lg_id: ESPN league ID
+    :return: full parsed dataframe of managers
+    """
     # fetch data
-    driver.get(espn_lg_member_base_url + lg_id)
-    # print("successfully navigated to " + league_member_base_url + lgID)
+    driver.get(ESPN_LG_BASE_URL + lg_id)
     driver.implicitly_wait(5)
-    # get managers table
+    # find managers table
     try:
         managers_table = WebDriverWait(driver, 7).until(
             EC.presence_of_element_located((By.CLASS_NAME, "Table"))).get_attribute("outerHTML")
-    except Exception as e:
-        exit(f"ERROR: could not find managers table. {e}")
+    except TimeoutException as te:
+        logging.error("ERROR: could not find managers table. %s", te)
+        raise
 
-    # print("successfully found managers table")
     driver.quit()
-    # print("closed chrome driver")
 
-    df = parse_managers(managers_table)
-
-    export_managers_json(df)
+    return parse_managers(managers_table)
 
 
-def export_managers_json(df):
-    # find the system path to /Shared/BaseballHQ/resources
-    directory = '/Users/Shared/BaseballHQ/resources/extract'
-    filename = 'lgmngrs.json'
-    full_path = os.path.join(directory, filename)
-    # write to json file
-    # orienting on table adds schema information to the json file
-    df.to_json(full_path, index=False, orient="table", indent=2)
-    # print("JSON file created successfully...")
-
-
-def parse_managers(managers_table):
+def parse_managers(managers_table) -> pd.DataFrame:
+    """
+    Parse the managers table
+    :param managers_table: HTML div table containing managers
+    :return: pandas dataframe of managers
+    """
     # Parse the HTML using BeautifulSoup
     soup = BeautifulSoup(managers_table, 'html.parser')
-    # find the column index for the table header "MANAGER NAME"
-    # Find the table header associated with the desired column
     desired_header = 'MANAGER NAME'
     header_cell = soup.find('th', string=desired_header)
     # Find the index of the header cell among its siblings
     column_index = header_cell.parent.index(header_cell)
-    # print("found column index of " + desired_header + ": " + str(column_index))
-    # Get the table rows
     table_rows = soup.find("tbody").find_all("tr")  # tbody is used to skip the header row
-    # Create a new DataFrame
+
     df = pd.DataFrame(columns=["teamAbbreviation", "teamName", "teamOwner", "avatarURL"])
-    # Print the found elements
+
     for team in table_rows:
-        tmDetails = [team.find(class_="teamAbbrev").text, team.find(class_="teamName").text,
+        team_details = [team.find(class_="teamAbbrev").text, team.find(class_="teamName").text,
                      team.find_all('td')[column_index].text,
                      team.find(class_=["image-custom", "team-logo"]).find("img")["src"]]
 
         # Create a Series to append
-        new_row = pd.Series(tmDetails, index=df.columns)
+        new_row = pd.Series(team_details, index=df.columns)
         # Use loc indexer to append the Series to the DataFrame
         df.loc[len(df)] = new_row
-        # print("successfully added " + tmDetails[1] + " to dataframe")
+
     return df
