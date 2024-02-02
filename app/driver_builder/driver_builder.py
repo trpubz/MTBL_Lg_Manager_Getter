533f28d74fc29a5750afbdfba459722565a00d0a
Taylor Pubins <tpubz@icloud.com>
2024-02-01 20:33:05 -0700

    Update driverkit to v0.3.2 --fixes root directory

--- app/driver_builder/driver_builder.py
+++ app/driver_builder/driver_builder.py
@@ -4,6 +4,7 @@ modified date: 30 JAN 2024
 description: module that builds the driver
 v0.1.0
 """
+import os
 from mtbl_driverkit import mtbl_driverkit as DK
 from selenium import webdriver
 
@@ -14,5 +15,5 @@ def build_driver(headless=True) -> webdriver.Chrome:
     :param headless: True or False
     :return: selenium webdriver
     """
-    driver, _ = DK.dk_driver_config(headless=headless)
+    driver, _ = DK.dk_driver_config(os.getcwd(), headless=headless)
     return driver
